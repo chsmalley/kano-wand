39c03a26d10a1dd767cf0f83f483660045518080
Chris Smalley <chris.smalley.32@gmail.com>
2021-12-26 16:07:44 -0500

    started files to learn spells

--- learn_spells.py
+++ learn_spells.py
@@ -5,29 +5,49 @@ from typing import Tuple
 import pandas as pd
 import numpy as np
 import math
+# pd.options.plotting.backend = "plotly"
 
-Quaternion = Tuple(float, float, float, float)
+Quaternion = Tuple[float, float, float, float]
+
+def make_quat(x: pd.Series):
+    return np.array((x["x"], x["y"], x["z"], x["w"]))
 
 def plot_spell(filename: str):
     title = os.path.splitext(
         os.path.basename(filename))[0]
-    df = pd.read_csv(filename, index="time")
-    df.plot(title=filename)
+    df = pd.read_csv(filename, index_col="time")
+    # Add distance to dataframe
+    df["q0"] = df.apply(func=make_quat, axis=1)
+    df["q1"] = df["q0"].shift(1)
+    # df["dist1"] = df.apply(func=angle_distance, axis=1)
+    df["dist2"] = df.apply(func=euclidean_distance, axis=1)
+    print(df)
+    # df["dist"] = df.apply()
+    fig = df.plot(title=filename).get_figure()
+    fig.savefig("tmp.png")
+    fig.show()
     # df.plot(x="time", )
 
-def angle_distance(q0: Quaternion, q1: Quaternion):
+def angle_distance(row: pd.Series):
     """
     Assuming these are unit quaternions used to represent rotations
     """
-    theta = math.acos(np.dot(np.array(q0), np.array(q1)))
+    if not isinstance(row["q1"], np.ndarray):
+        return np.nan
+    theta = math.acos(np.dot(row["q0"],
+                             row["q1"]))
     if theta > math.pi / 2:
         theta = math.pi - theta
     return theta
 
-def euclidean_distance(q0: Quaternion, q1: Quaternion):
-    return math.dist(q0, q1)
+def euclidean_distance(row: pd.Series):
+    if not isinstance(row["q1"], np.ndarray):
+        return np.nan
+    else:
+        return math.dist(row["q0"], row["q1"])
 
 if __name__ == '__main__':
     dirname = sys.argv[1]
     for filename in glob.glob("*.csv"):
         print(filename)
+        plot_spell(filename)
