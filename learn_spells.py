bc6ab73b6c84a09c91cf04648742800893c3fa55
Chris Smalley <chris.smalley@toyota.com>
2022-01-24 09:12:21 -0500

    updated learn spells to be able to compute the distances between all spells in two directories

--- learn_spells.py
+++ learn_spells.py
@@ -1,19 +1,45 @@
+"""
+How to run
+cd into repo
+$ python learn_spells.py test_data/spells1/ test_data/spells3/
+Note: spells1 and spells3 were captured by the same person. 
+spells2 was captured by a different person on the first time looking at the 
+spell chart. Some mistakes may have been made following the spell chart.
+"""
+
 import os
 import sys
 import glob
-from typing import Tuple, List
+import json
+from typing import Tuple, List, Dict
 import pandas as pd
 import numpy as np
 import math
 from scipy.spatial.transform import Rotation as R
 from scipy.spatial import distance
-pd.options.plotting.backend = "plotly"
+# pd.options.plotting.backend = "plotly"
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import classification_report
+from scipy.stats import mode
 
 Quaternion = Tuple[float, float, float, float]
+
+LABELS = {
+    1: 'AGUAMENTI',
+    2: 'AVIS',
+    3: 'ENGORGIO',
+    4: 'EXPELLIARMUS',
+    5: 'FLIPENDO',
+    6: 'INCENDIO',
+    7: 'LOCOMOTOR',
+    8: 'LUMOS',
+    9: 'REDUCIO',
+    10: 'REDUCTO',
+    11: 'STUPEFY',
+    12: 'WINGARDIUM_LEVIOSA'
+}
 
 def make_quat(x: pd.Series):
     return np.array((x["x"], x["y"], x["z"], x["w"]))
@@ -21,9 +47,10 @@ def make_quat(x: pd.Series):
 def dtw_distance(
     ts_a,
     ts_b,
-    d = lambda x,y: abs(x-y)
-    max_warping_window = 10000,
-): -> float
+    d = lambda x, y: math.dist(x, y),
+    # d = lambda x, y: abs(x - y),
+    max_warping_window:int = 10000,
+) -> float:
     """Returns the DTW similarity distance between two 2-D
     timeseries numpy arrays.
 
@@ -34,7 +61,7 @@ def dtw_distance(
         whose DTW distance between each sample of A and B
         will be compared
     
-    d : DistanceMetric object (default = abs(x-y))
+    d : DistanceMetric object (default = math.dist(x, y))
         the distance measure used for A_i - B_j in the
         DTW dynamic programming function
     
@@ -46,19 +73,20 @@ def dtw_distance(
     # Create cost matrix via broadcasting with large int
     ts_a, ts_b = np.array(ts_a), np.array(ts_b)
     M, N = len(ts_a), len(ts_b)
-    cost = sys.maxint * np.ones((M, N))
+    cost = sys.maxsize * np.ones((M, N))
 
     # Initialize the first row and column
+    # print(f"dist {ts_a[0]}, {ts_b[0]}")
     cost[0, 0] = d(ts_a[0], ts_b[0])
-    for i in xrange(1, M):
+    for i in range(1, M):
         cost[i, 0] = cost[i-1, 0] + d(ts_a[i], ts_b[0])
 
-    for j in xrange(1, N):
+    for j in range(1, N):
         cost[0, j] = cost[0, j-1] + d(ts_a[0], ts_b[j])
 
     # Populate rest of cost matrix within window
-    for i in xrange(1, M):
-        for j in xrange(max(1, i - max_warping_window),
+    for i in range(1, M):
+        for j in range(max(1, i - max_warping_window),
                         min(N, i + max_warping_window)):
             choices = cost[i - 1, j - 1], cost[i, j-1], cost[i-1, j]
             cost[i, j] = min(choices) + d(ts_a[i], ts_b[j])
@@ -66,9 +94,9 @@ def dtw_distance(
     # Return DTW distance given window 
     return cost[-1, -1]
 
-def dist_matrix(x, y, subsample_step=1):
+def distance_matrix(x, y, subsample_step=1):
     """Computes the M x N distance matrix between the training
-    dataset and testing dataset (y) using the DTW distance measure
+    dataset (x) and testing dataset (y) using the DTW distance measure
     
     Arguments
     ---------
@@ -83,15 +111,14 @@ def dist_matrix(x, y, subsample_step=1):
     
     # Compute the distance matrix        
     dm_count = 0
-    
     # Compute condensed distance matrix (upper triangle) of pairwise dtw distances
     # when x and y are the same array
     if(np.array_equal(x, y)):
         x_s = np.shape(x)
         dm = np.zeros((x_s[0] * (x_s[0] - 1)) // 2, dtype=np.double)
         
-        for i in xrange(0, x_s[0] - 1):
-            for j in xrange(i + 1, x_s[0]):
+        for i in range(0, x_s[0] - 1):
+            for j in range(i + 1, x_s[0]):
                 dm[dm_count] = dtw_distance(x[i, ::subsample_step],
                                             y[j, ::subsample_step])
                 dm_count += 1
@@ -107,22 +134,28 @@ def dist_matrix(x, y, subsample_step=1):
         dm = np.zeros((x_s[0], y_s[0])) 
         dm_size = x_s[0]*y_s[0]
     
-        for i in xrange(0, x_s[0]):
-            for j in xrange(0, y_s[0]):
-                dm[i, j] = self._dtw_distance(x[i, ::self.subsample_step],
-                                              y[j, ::self.subsample_step])
-                # Update progress bar
+        for i in range(0, x_s[0]):
+            for j in range(0, y_s[0]):
+                dm[i, j] = dtw_distance(x[i, ::subsample_step],
+                                        y[j, ::subsample_step])
                 dm_count += 1
         return dm
 
-def predict(self, x):
+def predict(
+    dist_matrix,
+    x,
+    labels: List[str],
+    n_neighbors=1
+    ):
     """Predict the class labels or probability estimates for 
     the provided data
 
     Arguments
     ---------
+      
       x : array of shape [n_samples, n_timepoints]
           Array containing the testing data set to be classified
+      
       
     Returns
     -------
@@ -130,19 +163,18 @@ def predict(self, x):
           (1) the predicted class labels 
           (2) the knn label count probability
     """
-    
-    dm = self._dist_matrix(x, self.x)
 
     # Identify the k nearest neighbors
-    knn_idx = dm.argsort()[:, :self.n_neighbors]
+    knn_idx = dist_matrix.argsort()[:, :n_neighbors]
 
     # Identify k nearest labels
-    knn_labels = self.l[knn_idx]
+    print(f"knn_idx: {knn_idx}")
+    knn_labels = labels[knn_idx]
     
     # Model Label
     mode_data = mode(knn_labels, axis=1)
     mode_label = mode_data[0]
-    mode_proba = mode_data[1]/self.n_neighbors
+    mode_proba = mode_data[1] / n_neighbors
 
     return mode_label.ravel(), mode_proba.ravel()
 
@@ -150,28 +182,37 @@ def predict(self, x):
 def make_euler(x: pd.Series):
     r = R.from_quat(x["q0"])
     euler = r.as_euler('zyx', degrees=True)
-    print(f"euler: {euler}")
+    # print(f"euler: {euler}")
     return euler
 
-def plot_spell(filename: str):
-    title = os.path.splitext(
-        os.path.basename(filename))[0]
-    df = pd.read_csv(filename, index_col="time")
-    # Add distance to dataframe
-    df["q0"] = df.apply(func=make_quat, axis=1)
-    df["q1"] = df["q0"].shift(1)
-    df["euler"] = df.apply(func=make_euler, axis=1)
-    # df["dist1"] = df.apply(func=angle_distance, axis=1)
-    # df["dist2"] = df.apply(func=euclidean_distance, axis=1)
-    print(df)
+def learn_spell(train_spells: Dict[str, str], test_spells: Dict[str, str]):
+    spell_results = {}
+    for train_spell, train_spell_file in train_spells.items():
+        train_df = pd.read_csv(train_spell_file, index_col="time")
+        train_df["q0"] = train_df.apply(func=make_quat, axis=1)
+        train_array = train_df["q0"].to_numpy()
+        train_array = train_array.reshape((1, train_array.shape[0]))
+        spell_results[train_spell] = {}
+        for test_spell, test_spell_file in test_spells.items():
+            test_df = pd.read_csv(test_spell_file, index_col="time")
+            # Add distance to dataframe
+            test_df["q0"] = test_df.apply(func=make_quat, axis=1)
+            test_array = test_df["q0"].to_numpy()
+            test_array = test_array.reshape((1, test_array.shape[0]))
+            dist_matrix = distance_matrix(train_array, test_array)
+            spell_results[train_spell][test_spell] = dist_matrix[0][0]
+            # a, b = predict(dist_matrix, test_array, np.array([1]))
+            # print(f"predict results: {a} {b}")
+    print(json.dumps(spell_results, indent=4))
     # Test dtw
-    dist = DTW(df["q0"], df["q1"])
-
+    # dist = DTW(train_df["q0"], test_df["q0"])
+    # dist = DTW(np.array([1] * 100), np.array([1] * 100))
+    # print(f"dtw dist: {dist}")
     # df["dist"] = df.apply()
-    fig = df.plot(title=filename)
+    # fig = df.plot(title=filename)
     # fig = df.plot(title=filename).get_figure()
     # fig.savefig("tmp.png")
-    fig.show()
+    # fig.show()
     # df.plot(x="time", )
 
 def angle_distance(row: pd.Series):
@@ -187,10 +228,10 @@ def angle_distance(row: pd.Series):
     return theta
 
 #custom metric
-def DTW(a: List[float], b: List[float]):   
+def DTW(a: List[float], b: List[float]):
     an = a.size
     bn = b.size
-    pointwise_distance = distance.cdist(a.reshape(-1,1),b.reshape(-1,1))
+    pointwise_distance = distance.cdist(a.reshape(-1,1), b.reshape(-1,1))
     cumdist = np.matrix(np.ones((an+1,bn+1)) * np.inf)
     cumdist[0,0] = 0
 
@@ -210,8 +251,14 @@ def euclidean_distance(row: pd.Series):
         return math.dist(row["q0"], row["q1"])
 
 if __name__ == '__main__':
-    dirname = sys.argv[1]
-    print(f"dirname: {dirname}")
-    for filename in glob.glob(dirname  + "*.csv"):
-        print(filename)
-        plot_spell(filename)
+    train_dirname = sys.argv[1]
+    test_dirname = sys.argv[2]
+    train_spells = {}
+    for filename in glob.glob(train_dirname + "*.csv"):
+        name = os.path.splitext(os.path.basename(filename))[0]
+        train_spells[name] = filename
+    test_spells = {}
+    for filename in glob.glob(test_dirname + "*.csv"):
+        name = os.path.splitext(os.path.basename(filename))[0]
+        test_spells[name] = filename
+    learn_spell(test_spells, train_spells)
