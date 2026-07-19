5b89e04744e4067573f261d37f55ec994fc16c72
Chris Smalley <chris.smalley.32@gmail.com>
2026-07-19 11:30:50 -0400

    adding orientation

--- read_wand_button.py
+++ read_wand_button.py
@@ -3,8 +3,16 @@ from bleak import BleakClient, BleakScanner
 
 # BUTTON_UUID = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
 BUTTON_UUID = '64A7000D-F691-4B93-A6F4-0968F5B648F8'
+SERVICE_UUID = '64A70011-F691-4B93-A6F4-0968F5B648F8'
+QUATERNIONS_UUID = '64A70002-F691-4B93-A6F4-0968F5B648F8'
+MOTION_UUID = '64A7000C-F691-4B93-A6F4-0968F5B648F8'
+MAGN_CALIBRATE_UUID = '64A70021-F691-4B93-A6F4-0968F5B648F8'
+QUATERNIONS_RESET_UUID = '64A70004-F691-4B93-A6F4-0968F5B648F8'
 
 def button_handler(sender, data):
+    print("Button data:", list(data))
+
+def orientation_handler(sender, data):
     print("Button data:", list(data))
 
 async def main():
@@ -20,6 +28,7 @@ async def main():
         print("Connected")
 
         await client.start_notify(BUTTON_UUID, button_handler)
+        await client.start_notify(QUATERNIONS_UUID, orientation_handler)
 
         print("Listening...")
         await asyncio.sleep(300)
