4088410392dbc682b541a7b53fbbfe94ea199228
Chris Smalley <chris.smalley.32@gmail.com>
2026-09-20 17:50:33 -0400

    fixed bug in nerf shooter

--- nerf/nerf_shooter.py
+++ nerf/nerf_shooter.py
@@ -77,9 +77,7 @@ def main():
     barrel_motor = Motor(forward=BARREL_MOTOR_FORWARD_PIN,
                          backward=BARREL_MOTOR_BACKWARD_PIN)
     pitch_motor = Motor(forward=PITCH_MOTOR_FORWARD_PIN,
-                        backward=PITCH_MOTOR_FORWARD_PIN)
-    max_pitch = Button(MAX_PITCH_PIN)
-    min_pitch = Button(MIN_PITCH_PIN)
+                        backward=PITCH_MOTOR_BACKWARD_PIN)
     
     print("Nerf shooter")
     shoot_dart(trigger_motor, barrel_motor, jam_button)
