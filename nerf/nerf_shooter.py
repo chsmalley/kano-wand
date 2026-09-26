ea16c8633ed9bc1e710ab2e5bea5a0ef9bddd927
chris smalley <chris.smalley.32@gmail.com>
2026-09-26 18:33:43 -0400

    fixed pins which what is actually connected

--- nerf/nerf_shooter.py
+++ nerf/nerf_shooter.py
@@ -2,8 +2,8 @@ from gpiozero import Motor, Button
 import time
 
 # CONSTANTS
-BARREL_SPEED = 0.5
-TRIGGER_SPEED = 0.4
+BARREL_SPEED = 0.4
+TRIGGER_SPEED = 0.3
 TRIGGER_DELAY = 0.4
 TRIGGER_TIME = 0.7
 TRIGGER_TIMEOUT = 2
@@ -11,18 +11,18 @@ PITCH_SPEED = 0.5
 PITCH_TIME = 0.5
 
 # GPIO PINS
-TRIGGER_MOTOR_FORWARD_PIN = 23
-TRIGGER_MOTOR_BACKWARD_PIN = 24
+TRIGGER_MOTOR_FORWARD_PIN = 6
+TRIGGER_MOTOR_BACKWARD_PIN = 5
 PITCH_MOTOR_FORWARD_PIN = 14
 PITCH_MOTOR_BACKWARD_PIN = 15
-BARREL_MOTOR_FORWARD_PIN = 27
-BARREL_MOTOR_BACKWARD_PIN = 22
+BARREL_MOTOR_FORWARD_PIN = 2
+BARREL_MOTOR_BACKWARD_PIN = 3
 # PITCH PINS
-MAX_PITCH_PIN = 2
-MIN_PITCH_PIN = 3
+MAX_PITCH_PIN = 22
+MIN_PITCH_PIN = 23
 # SHOOTER PINS
 JAM_DOOR_PIN = 20
-JAM_PIN = 21
+JAM_PIN = 13
 
 
 def shoot_dart(trigger: Motor,
@@ -39,7 +39,7 @@ def shoot_dart(trigger: Motor,
     timeout_time = time.time() + TRIGGER_TIMEOUT
     prev_trigger_pos = False
     curr_trigger_pos = False
-    while not nerf_shot and time.time() < timeout_time:
+    while time.time() < timeout_time:
         print(f"trigger state: {jam_button.is_pressed}")
         prev_trigger_pos = curr_trigger_pos
         curr_trigger_pos = jam_button.is_pressed
@@ -69,15 +69,15 @@ def lower_motor(pitch: Motor, min_pitch: Button) -> None:
 def main():
     # Create Motor objects
     jam_button = Button(JAM_PIN)
-    door_button = Button(JAM_DOOR_PIN)
-    max_pitch = Button(MAX_PITCH_PIN)
-    min_pitch = Button(MIN_PITCH_PIN)
+    # door_button = Button(JAM_DOOR_PIN)
+    # max_pitch = Button(MAX_PITCH_PIN)
+    # min_pitch = Button(MIN_PITCH_PIN)
     trigger_motor = Motor(forward=TRIGGER_MOTOR_FORWARD_PIN,
                           backward=TRIGGER_MOTOR_BACKWARD_PIN)
     barrel_motor = Motor(forward=BARREL_MOTOR_FORWARD_PIN,
                          backward=BARREL_MOTOR_BACKWARD_PIN)
-    pitch_motor = Motor(forward=PITCH_MOTOR_FORWARD_PIN,
-                        backward=PITCH_MOTOR_BACKWARD_PIN)
+    # pitch_motor = Motor(forward=PITCH_MOTOR_FORWARD_PIN,
+    #                     backward=PITCH_MOTOR_BACKWARD_PIN)
     
     print("Nerf shooter")
     shoot_dart(trigger_motor, barrel_motor, jam_button)
